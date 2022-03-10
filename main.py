202bdc77e3ec2be17fe7d7c8e7706056f78d7c7b
Jhebi <jb11malabanan@gmail.com>
2022-03-10 21:42:05 +0800

    Some comments

---
+++ main.py
@@ -0,0 +1,5 @@
+#encrypted database prototype before making password saver app
+#since sqlite does not have a master password, you can't hide encryption key behind a locked database
+#instead the master password is the encryption key itself
+#if you hash the master password, you will get the encryption key
+
