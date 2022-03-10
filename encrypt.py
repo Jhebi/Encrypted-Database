af364074943f589ce2c0bb124b778c89d3761585
Jhebi <jb11malabanan@gmail.com>
2022-03-10 23:17:58 +0800

    Created a hash function

--- encrypt.py
+++ encrypt.py
@@ -1,5 +1,2 @@
 #Original key: 
-#ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~`!@#$%^&*()_-+=\"{[}]|\:;'<,>.?/\\
-
-#Encryption key: 
-#oh8Es"B(:cwVu6C;zXi2.nQWH~$4r1T_Nf-b+S%Aqlj'[Kd!YvI,|gZR/9Dxy}\atkpU\G@*05])=OP&e?F{^7m#3`LJ<>M
+#ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~`!@#$%^&*()_-+={[}]|:'<,>.?/
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 #since sqlite does not have a master password, you can't hide encryption key behind a locked database
 #instead the master password is the encryption key itself
 #if you hash the master password, you will get the encryption key
+#master password jhebi
 
 import hash
 
