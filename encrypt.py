deb20941c1bc931fa72d3fb680e6558faefa78ab
Jhebi <jb11malabanan@gmail.com>
2022-03-11 20:36:27 +0800

    Rough finish on main + define encrypt functions

--- encrypt.py
+++ encrypt.py
@@ -1,2 +1,8 @@
 #Original key: 
 #ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~`!@#$%^&*()_-+={[}]|:'<,>.?/
+
+def insert(key, name, password):
+    return
+
+def retrieve(key, name):
+    return
--- main.py
+++ main.py
@@ -7,5 +7,30 @@
 #encrypt.py allows you to insert and retrieve data from database
 
 import hash
+import encrypt
 
+#get the master password
+print("Please Enter Master password:", end = " ")
+masterpass = input()
+if masterpass != "jhebi":
+    print("Error")
+    quit()
 
+#hash the password to get the encryption key
+key = hash.hash(masterpass)
+
+#insert to database or retrieve?
+print("(1) insert \n(2) retrieve")
+choice = input()
+if choice == 1:
+    print("Enter name: ", end = "")
+    name = input()
+    print("Enter password: ", end = "")
+    password = input()
+    encrypt.insert(key, name, password)
+elif choice == 2:
+    print("Enter name: ", end = "")
+    name = input()
+    encrypt.retrieve(key, name)
+else:
+    print("Invalid Choice")
